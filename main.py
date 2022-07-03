a153a457b6418e0edc0c55a713cdc06ebae857f0
Fwazle <NolanIS@users.noreply.github.com>
2022-07-02 20:46:11 -0700

    Single Qubit implementation of Deutsch algorithm
    I tried to comment on everything and make it as readable as possible, if anything is confusing let me know
    
    I implemented deutsch algorithm as a function that takes in an oracle and composes them together. The deutsch algorithm measures the first Qubit, collapsing the state.
    
    matplotlib is required for drawing the program
    pylatexenc is required for visualizing the program

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# Sample Hello World code, works in PyCharm on a Conda environment with Python 3.10 and Qiskit installed
-# Unsure if it works with other environments
-
 import qiskit
+import matplotlib.pyplot as plt
 
-qr = qiskit.QuantumRegister(2)
-cr = qiskit.ClassicalRegister(2)
-program = qiskit.QuantumCircuit(qr, cr)
+# Deutsch algorithm for a single qubit
+# Takes in an Oracle (QuantumCircuit) with 2 qubits
+def deutsch(oracle):
+    program = qiskit.QuantumCircuit(2, 1)  # 2 qubits, 1 bit
+    program.x(1)  # Prepare qubit 2 into state |1>
 
-program.h(1)
-program.measure(qr,cr)
+    program.h(0)  # Hadamard all qubits
+    program.h(1)
 
-print(program)
+    program.barrier() # Making it look better, idea from https://qiskit.org/textbook/ch-algorithms/deutsch-jozsa.html
 
+    program.compose(oracle, inplace=True)
+
+    program.barrier()
+
+    program.h(0)  # Hadamard the first part
+    program.measure(0, 0)  # Measure the first part (0) into the only classical register (0)
+    return program
+
+
+# Defining an Oracle
+oracle = qiskit.QuantumCircuit(2)       # 2 qubits, 0 bits
+oracle.cnot(0,1)
+
+# Adding the oracle to the deutsch algorithm
+program = deutsch(oracle)
+
+# Visualizing the circuit
+program.draw(output='mpl')
+plt.show()
+
+# Running the circuit, getting the results
 job = qiskit.execute( program, qiskit.BasicAer.get_backend('qasm_simulator'))
 print( job.result().get_counts() )
